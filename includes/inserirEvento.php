2ecbfd33397fc95c95524842880060ff6de720a8
Gabriel Viana <gaa.vianaa@gmail.com>
2025-05-07 17:58:15 -0300

    validação e mascara js

--- includes/inserirEvento.php
+++ includes/inserirEvento.php
@@ -1,69 +1,73 @@
 <div class="formularios" id="formEvento">
-            <h2>Criar Novo Evento</h2>
+    <h2>Criar Novo Evento</h2>
 
-            <form>
-                <div class="form">
-                    <label for="nomeEvento">Nome do Evento</label>
-                    <input type="text" id="nomeEvento" name="nomeEvento" placeholder="Digite o nome do Evento" required>
-                </div>
-
-                <div class="form">
-                    <label for="descricaoEvento">Descrição</label>
-                    <textarea name="descricaoEvento" id="descricaoEvento" placeholder="Descreva o Evento" required></textarea>
-                </div>
-
-                <div class="form">
-                    <label for="dataEvento">Data</label>
-                    <input type="date" name="dataEvento" id="dataEvento" required>
-                </div>
-
-                <div class="form">
-                    <label for="horarioEvento">Horario</label>
-                    <input type="time" name="horarioEvento" id="horarioEvento" required>
-                </div>
-
-                <div class="form">
-                    <label for="cep">CEP</label>
-                    <div class="cep">
-                        <input type="text" name="cep" id="cep" placeholder="00000-000" required>
-                        <button type="button" id="buscarBtn" class="btnCEP">buscar</button>
-                    </div>
-                    <div id="resultado"></div>
-                </div>
-
-                <div class="form formCep">
-                    <label for="rua">Rua</label>
-                    <input type="text" id="rua" name="rua" required>
-                </div>
-
-                <div class="form formCep">
-                    <label for="complemento">complemento</label>
-                    <input type="text" id="complemento" name="complemento">
-                </div>
-
-                <div class="form formCep">
-                    <label for="bairro">Bairro</label>
-                    <input type="text" id="bairro" name="bairro" required>
-                </div>
-
-                <div class="form formCep">
-                    <label for="cidade">Cidade</label>
-                    <input type="text" id="cidade" name="cidade" required>
-                </div>
-
-                <div class="form formCep">
-                    <label for="estado">Estado</label>
-                    <input type="text" id="estado" name="estado" required>
-                </div>
-
-                <div class="form">
-                    <label for="celularEvento">Telefone</label>
-                    <input type="tel" id="celular" name="celularEvento" required placeholder="(00) 00000-0000">
-                </div>
-
-                <div class="form">
-                    <label for="imagemEvento">Imagem do Evento</label>
-                    <input type="file" id="imagemEvento" name="imagemEvento" accept="image/*" required>
-                </div>
-            </form>
+    <form>
+        <div class="form">
+            <label for="nomeEvento">Nome do Evento</label>
+            <input type="text" id="nomeEvento" name="nomeEvento" placeholder="Digite o nome do Evento" required>
         </div>
+
+        <div class="form">
+            <label for="descricaoEvento">Descrição</label>
+            <textarea name="descricaoEvento" id="descricaoEvento" placeholder="Descreva o Evento" required></textarea>
+        </div>
+
+        <div class="form">
+            <label for="dataEvento">Data</label>
+            <input type="date" name="dataEvento" id="dataEvento" required>
+        </div>
+
+        <div class="form">
+            <label for="horarioEvento">Horario</label>
+            <input type="time" name="horarioEvento" id="horarioEvento" required>
+        </div>
+
+        <div class="form">
+            <label for="cep">CEP</label>
+            <div class="cep">
+                <input type="text" name="cep" id="cep" placeholder="00000-000" required>
+                <button type="button" id="buscarBtn" class="btnCEP">buscar</button>
+            </div>
+            <div id="resultado"></div>
+        </div>
+
+        <div class="form formCep">
+            <label for="rua">Rua</label>
+            <input type="text" id="rua" name="rua" required>
+        </div>
+
+        <div class="form formCep">
+            <label for="complemento">complemento</label>
+            <input type="text" id="complemento" name="complemento">
+        </div>
+
+        <div class="form formCep">
+            <label for="bairro">Bairro</label>
+            <input type="text" id="bairro" name="bairro" required>
+        </div>
+
+        <div class="form formCep">
+            <label for="cidade">Cidade</label>
+            <input type="text" id="cidade" name="cidade" required>
+        </div>
+
+        <div class="form formCep">
+            <label for="estado">Estado</label>
+            <input type="text" id="estado" name="estado" required>
+        </div>
+
+        <div class="form">
+            <label for="celularEvento">Telefone</label>
+            <input type="tel" id="celular" name="celularEvento" required placeholder="(00) 00000-0000">
+        </div>
+
+        <div class="form">
+            <label for="imagemEvento">Imagem do Evento</label>
+            <input type="file" id="imagemEvento" name="imagemEvento" accept="image/*" required>
+        </div>
+
+        <div class="form">
+            <button type="submit" class="btnEnviar">Enviar Projeto</button>
+        </div>
+    </form>
+</div>
