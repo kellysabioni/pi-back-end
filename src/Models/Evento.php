df8e6698339e14e1840f7c2a9fdb836dc1cb8299
dcarianela <aki.hayakawa5577@gmail.com>
2025-05-08 11:06:37 -0300

    cria classe Evento

--- src/Models/Evento.php
+++ src/Models/Evento.php
@@ -1,3 +1,23 @@
 <?php 
 namespace ProjetaBD\Models;
 
+class Evento {
+    private int $id;
+    private string $nome;
+    private string $descricao;
+    private string $data;
+    private string $cep;
+    private string $rua;
+    private int $numero;
+    private string $bairro;
+    private string $cidade;
+    private string $uf;
+    private string $telefone;
+    private ?int $usuarios_id;
+    private ?int $projetos_id;
+
+    public function __construct(int $id, string $nome, string $descricao, string $data, string $cep, string $rua, string $numero, string $bairro, string $cidade, string $uf, string $telefone, ?int $usuarios_id = null, ?int $projetos_id = null )
+    {
+        
+    }
+}
