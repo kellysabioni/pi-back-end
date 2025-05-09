d50b40aee27c3724e2a25a64035d5e1b41f04b51
dcarianela <aki.hayakawa5577@gmail.com>
2025-05-09 11:09:56 -0300

    finaliza classe Evento e Usuario

--- src/Models/Usuario.php
+++ src/Models/Usuario.php
@@ -2,12 +2,12 @@
 namespace ProjetaBD\Models;
 
 class Usuario {
-    private int $id;
+    private ?int $id;
     private string $nome;
     private string $email;
     private string $senha;
 
-    public function __construct( int $id, string $nome, string $email, string $senha )
+    public function __construct( ?int $id = null, string $nome, string $email, string $senha )
     {
         $this->setId($id);
         $this->setNome($nome);
@@ -15,43 +15,13 @@ class Usuario {
         $this->setSenha($senha);
     }
 
-    public function getId(): int
-    {
-        return $this->id;
-    }
+    public function getId(): ?int {return $this->id;}
+    public function getNome(): string {return $this->nome;}
+    public function getEmail(): string {return $this->email;}
+    public function getSenha(): string {return $this->senha;}
 
-    public function getNome(): string
-    {
-        return $this->nome;
-    }
-    
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function getSenha(): string
-    {
-        return $this->senha;
-    }
-
-    private function setId(int $id): void
-    {
-        $this->id = $id;
-    }
-
-    private function setNome(string $nome): void
-    {
-        $this->nome = $nome;
-    }
-
-    private function setEmail(string $email): void
-    {
-        $this->email= $email;
-    }
-
-    private function setSenha(string $senha): void
-    {
-        $this->senha = $senha;
-    }
+    private function setId(?int $id): void {$this->id = $id;}
+    private function setNome(string $nome): void {$this->nome = $nome;}
+    private function setEmail(string $email): void {$this->email= $email;}
+    private function setSenha(string $senha): void {$this->senha = $senha;}
 }
