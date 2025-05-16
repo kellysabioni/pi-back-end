386eaa23640231051281d513a2811353eb3b9823
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-05-16 20:16:40 -0300

    Atualiza classe projetos com data criação e data update

--- src/Models/Projeto.php
+++ src/Models/Projeto.php
@@ -14,10 +14,12 @@ class Projeto{
     private string $UF;
     private string $telefone;
     private Categoria $categoria; 
+    private string $created_at;
+    private string $updated_at; 
     private ?int $usuarios_id;
     private ?int $eventos_id;
 
-    public function __construct(?int $id = null , string $nome, string $CEP, string $rua, string $numero, string $bairro, string $cidade, string $UF, string $telefone, Categoria $categoria = Categoria::Indefinido , ?int $usuarios_id = null, ?int $eventos_id = null) {
+    public function __construct(?int $id = null , string $nome, string $CEP, string $rua, string $numero, string $bairro, string $cidade, string $UF, string $telefone, Categoria $categoria = Categoria::Indefinido , string $created_at, string $updated_at , ?int $usuarios_id = null, ?int $eventos_id = null) {
         $this-> setId ($id);
         $this-> setNome ($nome);
         $this-> setCEP ($CEP);
@@ -28,6 +30,8 @@ class Projeto{
         $this-> setUF($UF);
         $this-> setTelefone($telefone);
         $this-> setCategoria ($categoria);
+        $this-> setCreatedAt (date('d/m/Y H:i:s'));
+        $this-> setUpdatedAt (date('d/m/Y H:i:s'));
         $this-> setUsuariosId ($usuarios_id);
         $this-> setEventosId ($eventos_id);
     }
@@ -42,6 +46,8 @@ class Projeto{
     public function getUF(): string {return $this->UF;}
     public function getTelefone(): string {return $this->telefone;}
     public function getCategoria(): Categoria {return $this->categoria;}
+    public function getCreatedAt(): string {return $this->created_at;}
+    public function getUpdatedAt(): string {return $this->updated_at;}
     public function getUsuariosId(): ?int {return $this->usuarios_id;}
     public function getEventosId(): ?int {return $this->eventos_id;}
 
@@ -55,6 +61,8 @@ class Projeto{
     private function setUF(string $UF): void { $this->UF = $UF;}
     private function setTelefone(string $telefone): void { $this->telefone = $telefone;}
     protected function setCategoria(Categoria $categoria): void { $this->categoria = $categoria;}
+    private function setCreatedAt(string $created_at): void { $this->created_at = $created_at;}
+    private function setUpdatedAt(string $updated_at): void { $this->updated_at = $updated_at;}
     private function setUsuariosId(?int $usuarios_id): void { $this->usuarios_id = $usuarios_id;}
     private function setEventosId(?int $eventos_id): void { $this->eventos_id = $eventos_id;}
 }
