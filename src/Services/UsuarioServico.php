2ba2cc35a9a5a7a86179a431de64720259718310
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-05-18 22:20:14 -0300

    Update UsuarioServico.php

--- src/Services/UsuarioServico.php
+++ src/Services/UsuarioServico.php
@@ -2,3 +2,33 @@
 
 namespace ProjetaBD\Services;
 
+use PDO;
+use ProjetaBD\Database\ConexaoBD;
+use ProjetaBD\Models\Usuario;
+
+class UsuarioServico
+{
+    private PDO $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = ConexaoBD::getConexao();
+    }
+
+    // inserir um registro de novo usuario
+    public function inserir(Usuario $usuario): void{
+        $sql = "
+        INSERT INTO usuarios (nome,email,senha)
+        VALUES (:nome,:usuario,:senha)";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $usuario->getNome(), PDO::PARAM_STR);
+            $consulta->bindValue(":usuario", $usuario->getEmail(), PDO::PARAM_STR);
+            $consulta->bindValue(":senha", password_hash($usuario->getSenha(), PASSWORD_DEFAULT), PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Throwable $erro) {
+            throw new Exception("Erro ao inserir usuário: " . $erro->getMessage());
+        }
+    }
+}
