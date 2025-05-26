2e1e43812f2d268a53ede61cac66dd727eda6f3a
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-05-26 11:25:35 -0300

    Cria pagina login erro

---
+++ includes/login-modal-erro.php
@@ -0,0 +1,25 @@
+<div id="loginModal" class="login-modal">
+    <div class="login-container">
+        <span class="login-fechar" onclick="fecharLoginModal()">&times;</span>
+        <div class="login-header">
+            <h1 class="alert alert-danger"> Usuário ou login inválido!</h1>
+            <h2 class="login-titulo">Entrar</h2>
+        </div>
+        <form id="loginForm" class="login-form" method="POST" action="index.php">
+            <div class="login-grupo">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="login-grupo">
+                <label for="senha">Senha</label>
+                <input type="password" id="senha" name="senha" required>
+            </div>
+            <div class="login-acoes">
+                <p>Ainda não tem uma conta?
+                    <a href="cadastrar.php" class="login-link >">Cadastrar-se</a> agora e aproveite todos os recursos!
+                </p>
+                <button name="enviar" type="submit" class="login-botao primario">Enviar</button>
+            </div>
+        </form>
+    </div>
+</div>
